fa55addd49a4c9bee2411e3e509b0b50d5e02833
Tobias Jacksteit <me@xtj7.de>
2020-12-30 22:37:46 +0800

    init

---
+++ kbrelay.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"github.com/eiannone/keyboard"
+)
+
+func main() {
+	keysEvents, err := keyboard.GetKeys(10)
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		_ = keyboard.Close()
+	}()
+
+	pressedKeys := []uint16{}
+
+	fmt.Println("Press ESC to quit")
+	for {
+		event := <-keysEvents
+		if event.Err != nil {
+			panic(event.Err)
+		}
+		fmt.Printf("You pressed: rune %q, key %X\r\n", event.Rune, event.Key)
+
+		if event.Key == keyboard.KeyF5 {
+			break
+		}
+	}
+}
