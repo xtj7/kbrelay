2d01032946370cd3ed5a1b9d05832a8c57531003
xtj7 <me@xtj7.com>
2021-03-29 00:20:41 +0800

    minor updates

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 stty -echo
-sudo go run $DIR/kbrelay.go "$@"
+sudo go run $DIR/kbrelay.go -map $DIR/maps/apple-magic-keyboard-numpad.json5 "$@"
 stty echo
